5fd3758ea5267ae9c9bcb150748ee3a05d7a19a5
divyaTesting <d.divya474@gmail.com>
2019-08-07 13:23:35 +0530

    intial commit

---
+++ TestingNewSDK/TestingNewSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestingNewSDK.h
+//  TestingNewSDK
+//
+//  Created by Ats on 07/08/19.
+//  Copyright © 2019 Ats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestingNewSDK.
+FOUNDATION_EXPORT double TestingNewSDKVersionNumber;
+
+//! Project version string for TestingNewSDK.
+FOUNDATION_EXPORT const unsigned char TestingNewSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingNewSDK/PublicHeader.h>
+
+
